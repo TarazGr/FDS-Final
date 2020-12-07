05901938eccd4a3d06272cbaad0cd13dc6a99aac
Gabriel-Radu Taranciuc <32823899+tarazgr@users.noreply.github.com>
2020-12-07 19:04:41 +0100

    added output parameter for train and test set saving

--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Preprocess and split data into training, testing and validation sets"
                                                  "This file handles all the feature engineering prior to training")
     parser.add_argument("dataset", help="The csv dataset to perform feature engineering on")
+    parser.add_argument('-o', '--output', default='datasets',
+                        help='folder path to save train, test and validations sets into')
     parser.add_argument('-c', '--columns', nargs='+', help='List of all columns names of the dataset')
     parser.add_argument('-f', '-x', '--features', nargs='+', help='Name of dataset columns to be handled as features')
     parser.add_argument('-lb', '-y', '--label', nargs='+', help='Name of the dataset column to be handled as label')
